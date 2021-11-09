d1742d875b022201fd64470d0184ab5b1195f362
shobbydun <shobbyduncan@gmail.com>
2021-11-09 11:44:37 +0300

    initial commit

--- app/src/main/java/com/example/recipeworld/MainActivity.kt
+++ app/src/main/java/com/example/recipeworld/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.recipeworld
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        val Btn_2 = findViewById<Button>(R.id.Btn_2)
+        Btn_2.setOnClickListener {
+            val Intent = Intent(this,sign_up::class.java)
+            startActivity(Intent)
+        }
+        val Btn_login = findViewById<Button>(R.id.Btn_login)
+        Btn_login.setOnClickListener {
+            val Intent=Intent(this,Recipes::class.java)
+            startActivity(Intent)
+
+        }
+
+
+
+
     }
+
+
+
+
 }
---
+++ app/src/main/java/com/example/recipeworld/Recipes.kt
@@ -0,0 +1,11 @@
+package com.example.recipeworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Recipes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipes)
+    }
+}
